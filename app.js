16d31a9c65447dc1c2b9a447e5270ac8ce1e2482
Kai Chen <kaiche2@cisco.com>
2016-03-12 16:08:42 -0800

    moved app.js file to the top of application hierarchy

---
+++ app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+//the empty array can hold module dependencies that will be available via dependency injection at run time
+var volleyballApp = angular.module('volleyTrackApp', []);
