c9918878873ddacc8f7f0807a8c4b54bd283ae11
Kai Chen <kaiche2@cisco.com>
2016-03-12 17:44:29 -0800

    Updated app.js to take advantage of app-controllers.js and implemented ngRoute (index.html, player-table.html, app.js, app-controllers.js) while updating bower package

---
+++ app-controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var volleyTrackControllers = angular.module('volleyTrackControllers', []);
+
+volleyTrackControllers.controller('MainCtrl', function($scope){
+	
+	$scope.players = [
+		{name: "Valerie Tam", city: 'Los Altos', id: 2},
+		{name: "Kai Chen", city: 'Fremont', id: 1},
+		{name: "Gabe Guerra", city: 'Fremont', id: 3}
+	];
+})
+
--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 'use strict';
 
 //the empty array can hold module dependencies that will be available via dependency injection at run time
-var volleyballApp = angular.module('volleyTrackApp', []);
+var volleyballApp = angular.module('volleyTrackApp', [
+	'ngRoute',
+	'volleyTrackControllers'
+]);
+
+volleyballApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.
+		when('/home', {
+			templateUrl: 'templates/player-table.html',
+			controller: 'MainCtrl'
+		}).
+		otherwise({
+			redirectTo: '/home'
+		});
+}]);
